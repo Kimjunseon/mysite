945bbf0c15be8af2767716222e0a0d27fd75959e
Kimjunseon <junseon0722@gmail.com>
2022-12-01 17:58:04 +0900

    d

--- mysite03/src/main/java/com/bitacademy/mysite/security/LoginInterceptor.java
+++ mysite03/src/main/java/com/bitacademy/mysite/security/LoginInterceptor.java
@@ -1,6 +1,6 @@
 package com.bitacademy.mysite.security;
 
-import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletRequest; 
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
